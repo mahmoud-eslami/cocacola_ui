c81c74f2681007320a2d01038efc1a21d96c49a8
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-04-17 10:35:07 +0430

    new Icon Added

--- lib/screen/bottom_nav/bottom_nav.dart
+++ lib/screen/bottom_nav/bottom_nav.dart
@@ -1,3 +1,4 @@
+import 'package:cocacolaui/resource/colors/colors.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavigation extends StatefulWidget {
@@ -10,9 +11,31 @@ class _BottomNavigationState extends State<BottomNavigation> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: Container(
+        height: 80,
         child: FittedBox(
-          child: FloatingActionButton(onPressed: (){}),
+          child: FloatingActionButton(
+            clipBehavior: Clip.antiAlias,
+            foregroundColor: AppColors.productScaffoldColor,
+            backgroundColor: AppColors.headerColor,
+            splashColor: AppColors.productHoverColor,
+            hoverColor: AppColors.headerColor,
+            elevation: 10,
+            onPressed: () {},
+            child: Image.asset(
+              'assets/images/soda.png',
+              height: 40,
+              width: 40,
+            ),
+          ),
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 75,
+        ),
+        notchMargin: 9,
       ),
     );
   }
