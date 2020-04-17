c4d0a633a27a5d37d28893cc59a9e22310a14dda
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-04-17 11:59:34 +0430

    Add a Shape with Canvas

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:cocacolaui/resource/colors/colors.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,6 +9,37 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 200,
+      width: MediaQuery.of(context).size.width,
+      child: CustomPaint(
+        painter: CurvePainter(),
+      )
+    );
   }
+}
+
+class CurvePainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = AppColors.headerColor;
+    paint.style = PaintingStyle.fill;
+
+    var path = Path();
+
+    path.moveTo(0, size.height * .25);
+    path.quadraticBezierTo(
+        size.width / 2, size.height / 2, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+
 }
