6bb3a932d6ec22c02c1d160a8f488ad31a011cfb
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-04-17 10:08:57 +0430

    Strings and Colors Added

---
+++ lib/resource/colors/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const headerColor = Color(0xffed1c24);
+  static const shareBackColor = Color(0xfff6e9d2);
+  static const darkTextColor = Color(0xff283144);
+  static const lightTextColor = Colors.white;
+  static const productBackColor = Color(0xffffe8e6);
+  static const productScaffoldColor = Color(0xfff2d5d4);
+  static const productHoverColor = Color(0xffecc4c2);
+}
---
+++ lib/resource/strings/strings.dart
@@ -0,0 +1,7 @@
+class Strings{
+  static const homeTitle = 'CocaCola';
+  static const shareTitle = 'Share a Coke';
+  static const shareDesc = 'Personalise your coke bottle';
+  static const productPartTitle = 'Our product';
+  static const addToCart = 'ADD TO CART';
+}
